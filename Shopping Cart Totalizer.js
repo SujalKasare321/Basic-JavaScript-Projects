19a9f7a209e88a33dae2062faaa1e26a165a222b
SujalKasare321 <sujalkasare321@gmail.com>
2026-02-25 01:35:17 +0530

    Add files via upload

---
+++ Shopping Cart Totalizer.js
@@ -0,0 +1,17 @@
+function calculateTotal(cart) {
+    let total = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        total += cart[i].price * cart[i].quantity;
+    }
+
+    return total;
+}
+const cart = [
+    { name: "Laptop", price: 50000, quantity: 1 },
+    { name: "Mouse", price: 500, quantity: 2 },
+    { name: "Keyboard", price: 1500, quantity: 1 }
+];
+
+console.log(calculateTotal(cart));
+
